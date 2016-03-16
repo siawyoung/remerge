db12177b9099fc71be48cab42e2b846b4c4453b2
Lau Siaw Young <lausiawyoung@gmail.com>
2016-03-16 15:40:48 +0800

    Replace entire lodash import with specific imports

--- src/mapReducers.js
+++ src/mapReducers.js
@@ -1,11 +1,11 @@
 
-import _ from 'lodash'
+import clone from 'lodash.clone'
 
 export const mapInsertReducer = (
   state = new Map(),
   action
 ) => {
-  const newState = _.clone(state)
+  const newState = clone(state)
   newState.set(action.insertKey, action.data)
   return newState
 }
@@ -14,7 +14,7 @@ export const mapDeleteReducer = (
   state,
   action
 ) => {
-  const newState = _.clone(state)
+  const newState = clone(state)
   newState.delete(action.deleteKey)
   return newState
 }
